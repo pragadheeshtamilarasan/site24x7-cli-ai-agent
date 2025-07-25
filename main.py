873a6202decb39677e3e2ddf9b777f4ae5da6fd2
praga_t <40648249+pragadheeshtamilarasan@users.noreply.github.com>
2025-07-25 23:17:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=5545,
+        port=8080,
         reload=False,
         access_log=True
     )
